8ce4b379a211a5b147a7003d72e20cb067883e82
AleksandraGorodnya <aleksandraskay@gmail.com>
2020-04-21 23:16:00 +0300

    Finished NewsApp project

--- app.js
+++ app.js
@@ -56,7 +56,131 @@ function CustomHttp() {
 //Init http module
 const http = CustomHttp();
 
+const newsService = (function() {
+  const apiKey = 'cc69721cb720422ea7bcf693d96cb860';
+  const apiUrl = 'http://newsapi.org/v2';
+
+  return {
+    topHeadlines(country = 'ua', cb) {
+      http.get(`${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`, cb);
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+
+//Elements
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const searchInput = form.elements['search'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  loadNews();
+});
+
 //Init selects
 document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
+  loadNews();
 });
+
+
+//Load news function
+function loadNews() {
+  showLoader();
+
+  const country = countrySelect.value;
+  const searchText = searchInput.value;
+
+  if (!searchText) {
+    newsService.topHeadlines(country, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
+}
+
+//Function on get response from server
+function onGetResponse(err, res) {
+   removePreloader();
+
+  if (err) {
+    showAlert(err, 'error-msg');
+    return;
+  }
+
+  if (!res.articles.length) {
+    return;
+  }
+  renderNews(res.articles);
+}
+
+//Function render news
+function renderNews(news) {
+  const newsContainer = document.querySelector('.news-container .row');
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+  let fragment = '';
+
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+  
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+
+//Function clear container
+function clearContainer(container) {
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+//News item template function
+function newsTemplate({ urlToImage, title, url, description }) {
+  return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage}">
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class="card-action">
+           <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function showAlert(msg, type = 'success') {
+  M.toast({ html: msg, classes: type });
+}
+
+// Show loader function
+function showLoader() {
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <div class="progress">
+      <div class="indeterminate"></div>
+    </div>
+  `,
+  );
+}
+
+//Remove loader function
+function removePreloader() {
+  const loader = document.querySelector('.progress');
+  if (loader) {
+    loader.remove();
+  }
+}
